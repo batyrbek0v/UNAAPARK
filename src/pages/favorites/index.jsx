fd55d0c7a651e1a3c939a5b4475b1cf5e1f9ce85
Erlan <erlankambarov94@gmail.com>
2022-06-22 14:43:38 +0600

    pushing for doni

--- src/pages/favorites/index.jsx
+++ src/pages/favorites/index.jsx
@@ -9,79 +9,78 @@ import Card from '../../components/Card/CarCards/Card'
 import Loader from '../../components/Loader'
 
 const Favorites = () => {
-  const [base, setBase] = React.useState()
-  const { users } = useAuth()
+	const [base, setBase] = React.useState()
+	const { users } = useAuth()
 
 
-  React.useEffect(() => {
-    getSavedCars(users && users.id)
-      .then(res => {
-        const result = Object.entries(res.data)
-          .map(([key, value]) => {
-            return {
-              id: key,
-              ...value
-            }
-          })
+	React.useEffect(() => {
+		getSavedCars(users && users.id)
+			.then(res => {
+				const result = Object.entries(res.data)
+					.map(([key, value]) => {
+						return {
+							id: key,
+							...value
+						}
+					})
 
-        setBase(result)
-      })
-  }, [users])
+				setBase(result)
+			})
+	}, [users])
 
-  const handleRemoveCar = (id) => {
-    console.log(id);
-    removeSavedCar(users.id, id)
-      .then(res => res
-        &&
-        getSavedCars(users && users.id)
-          .then(res => console.log(res.data))
-      )
-  }
+	const handleRemoveCar = (id) => {
+		removeSavedCar(users.id, id)
+		console.log(id)
+	}
 
 
+	if (base == null) return <h3>Вы еще ничего не сохраняли 😃</h3>
 
-  if (base == null) return <h3>Вы ничего не сохраняли :)</h3>
+	if (!base) return <Loader />
+	return (
+		<div className='savedCars'>
+			<h1>Ваши сохраненные машины</h1>
+			<div className='card_container'>
+				{
+					base && base.map(({ id, title, photo, price }) => (
+						<div to={`/carsmore/${id}`} className="cars_card" key={id}>
+							<div className="card_body">
+								<div className="card_img">
+									<img src={photo ? photo : notImage} alt={title} />
+								</div>
+								<div className='card_title'>
+									<h4>{title}</h4>
+									<h4>2015</h4>
+									<h4>{price} $ в сутки</h4>
 
-  if (!base) return <Loader />
-  console.log(base);
-
-  return (
-    <div className='savedCars'>
-      <h1>Ваши сохраненные машины</h1>
-      <div className='card_container'>
-        {
-          base && base.map(({ id, title, photo, price }) => (
-            <div className="cars_card" key={id}>
-              <button
-                className='favorites_btn'
-                onClick={() => {
-                  handleRemoveCar(id)
-                }}
-              >
-                <MdClose />
-              </button>
-              <div className="card_image">
-                <img src={photo ? photo : notImage} alt={title} />
-              </div>
-              <div className="card_body">
-                <h2>{title}</h2>
-                <div className="card_price">
-                  <h4>Цена:</h4>
-                  <p>
-                    {price}$
-                    <span> / в сутки</span>
-                  </p>
-                </div>
-              </div>
-              <div className="card_footer">
-                <Link className='card_footer_btn' to={`/carsmore/${id}`}>Подробнее</Link>
-              </div>
-            </div>
-          )).reverse()
-        }
-      </div>
-    </div>
-  )
+								</div>
+							</div>
+							<button
+								className='favorites_btn'
+								onClick={e => {
+									e.preventDefault()
+									handleRemoveCar(id)
+								}}
+							>
+								<MdClose />
+							</button>
+							<div className="card_footer">
+								<Link className='card_footer_btn' to={`/carsmore/${id}`}>Детали</Link>
+								<button
+									className='card_footer_btn'
+									onClick={() => {
+										window.open('https://t.me/sattarzanov')
+									}}
+								>
+									Забронировать
+								</button>
+							</div>
+						</div>
+					)).reverse()
+				}
+			</div>
+		</div>
+	)
 }
 
 export default Favorites
