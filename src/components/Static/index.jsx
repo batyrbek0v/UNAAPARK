4892cc82d14a1440110f14a65a7798842e0f1287
Erlan <erlankambarov94@gmail.com>
2022-06-27 18:31:54 +0600

    pulling

--- src/components/Static/index.jsx
+++ src/components/Static/index.jsx
@@ -3,6 +3,7 @@ import cls from './Static.module.scss'
 import ProgressCircle from '../ProgressCircle/ProgressCircle'
 import BetweenTitle from '../TitleForStatics/BetweenTitle'
 import { useCard } from '../hooks/useCard'
+import { useParams } from 'react-router-dom'
 
 
 
@@ -13,6 +14,8 @@ const Static = () => {
 
   const { base } = useCard()
 
+  const {id} = useParams()
+  const filter = base && base.filter(item => item.id === id ? item : '')
   console.log(base);
 
   return (
@@ -21,27 +24,23 @@ const Static = () => {
         <BetweenTitle color="black" />
         <div className={cls.stat_block}>
           {
-            base && base.map(base => <ProgressCircle percentage={base.maxSpeed} article="км" />).slice(0, 1)
+            filter && filter.map(base => <ProgressCircle percentage={base.maxSpeed} article="км"/>)
           }
           <p>МАХ.Скорость</p>
         </div>
         <div className={cls.stat_block}>
           {
-            base && base.map(base => <ProgressCircle percentage={base.power} article="л.с" />).slice(0, 1)
+            filter && filter.map(base => <ProgressCircle percentage={base.power} article="л.с" />)
           }
           <p>Мощность двигаетеля</p>
         </div>
         <div className={cls.stat_block}>
           {
-            <ProgressCircle percentage={43} article="л" />
+            // <ProgressCircle percentage={43} article="л" />
+            filter && filter.map(base => <ProgressCircle percentage={base.benz} article="л" />)
+
           }
           <p>Объем топливного бака</p>
-        </div>
-        <div className={cls.stat_block}>
-          {
-            <ProgressCircle percentage={289} article="" />
-          }
-          <p>Общая мощность</p>
         </div>
       </div>
     </React.Fragment>
